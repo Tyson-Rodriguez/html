75a3e92a3fb8247adef826a9d99a907c64a6498e
Tyson-Rodriguez <TysonRod17@gmail.com>
2016-10-25 12:11:50 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>. . ::LIMA:: . . </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--  There are no external links to css or js-->
+  <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="dist/css/custom.css">
+  <script src="dist/jquery/jquery.min.js"></script>
+  <script src="dist/js/bootstrap.min.js"></script>
+  <!-- *********************************** -->
+</head>
+<body>
+<!--  add a menu here -->
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: inline-block;
+    color: yellow;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover {
+    background-color: #000;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="phpcode">PHP Code</a></li>
+  <li><a href="downloads">Downloads</a></li>
+  <li><a href="javascript">Java Script</a></li>
+  <li><a href="templates">Templates</a></li>
+  <img src="img/lima.png" class="img-circle" alt="LIMA" width="20" height="20">
+</ul>
+
+</body>
+<body style="background-color:black;"></body>
+</html>
+
+
+
+<!-- end of menu if is the iframe-->
+
+<!-- do not change the following until you have met localhost checkoff #2 -->
+<div class="jumbotron text-center">
+  <h1>Web Page Requirement</h1>
+<body>
+
+
+
+</body>
+  
+  <p></p>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <div class="container">
+		<h2>Responsive Iframe</h2>
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe  class="embed-responsive-item" name="if" src="intro.html"  ></iframe>
+			</div>
+	</div>
+
+    </div>
+   
+</div>
+
+</body>
+</html>
